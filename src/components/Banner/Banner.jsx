6ffe750b3842ecf0dd976cb31f24598e9b3205dc
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-03-26 02:40:32 +0600

    added model of banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,13 +2,13 @@
 
 const Banner = () => {
     return (
-        <div className="container flex flex-col lg:flex-row border rounded-2xl bg-gray-200 gap-10 mt-10">
+        <div className="container mx-auto flex flex-col lg:flex-row border rounded-2xl bg-gray-200 gap-10 mt-10">
            
-            <div className="text-center flex flex-col justify-center gap-6  w-2/3">
-                <h1 className="text-6xl font-bold text-black">Books to freshen up your bookshelf</h1>
-                <button className="btn mt-3 bg-lime-500 text-white w-36 ml-36">View The List</button>
+            <div className="text-center flex flex-col justify-center gap-6  w-full lg:w-2/3">
+                <h1 className="text-6xl font-bold text-black pt-10 lg:pt-0">Books to freshen up your bookshelf</h1>
+                <button className="btn mt-3 bg-lime-500 text-white w-36 ml-56 lg:ml-36">View The List</button>
             </div>
-            <div className=" p-16 w-1/3">
+            <div className="ml-20 lg:ml-0  p-16 w-full lg:w-1/3">
                 <img src="https://i.ibb.co/h8Sx3qz/pngwing-1.jpg" alt="" />
             </div>
         </div>
