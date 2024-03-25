b6db94b644f190b67c0292689aa2b2de7331ff6e
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-03-26 02:13:29 +0600

    added navbar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,15 @@
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is home page banner</h2>
+        <div className="container flex flex-col lg:flex-row border rounded-2xl bg-gray-200 gap-10 mt-10">
+           
+            <div className="text-center flex flex-col justify-center gap-6  w-2/3">
+                <h1 className="text-6xl font-bold text-black">Books to freshen up your bookshelf</h1>
+                <button className="btn mt-3 bg-lime-500 text-white w-36 ml-36">View The List</button>
+            </div>
+            <div className=" p-16 w-1/3">
+                <img src="https://i.ibb.co/h8Sx3qz/pngwing-1.jpg" alt="" />
+            </div>
         </div>
     );
 };
---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,11 @@
+
+
+const Books = () => {
+    return (
+        <div className="mt-10">
+            <h1 className="text-6xl font-bold text-center">Books</h1>
+        </div>
+    );
+};
+
+export default Books;
