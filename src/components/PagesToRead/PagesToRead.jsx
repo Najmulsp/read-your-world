97274ce6b93e987a49697a9f6313177c8bdbb42a
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-03-28 18:06:18 +0600

    added custom shape chart

--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { useLoaderData } from "react-router-dom";
 import { getStoredReadBook } from "../../Utility/LocalStorage";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
+
 
 const PagesToRead = () => {
   const books = useLoaderData();
 
     const [readBooks, setReadBooks] = useState([]);
+
+   const [totalPages, bookName] = readBooks;
 
     useEffect(() => {
       const storedReadBooks = getStoredReadBook();
@@ -19,12 +26,25 @@ const PagesToRead = () => {
       }
     }, [books]);
 
+
+
+    const getPath = (x, y, width, height) => {
+      return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+      ${x + width / 2}, ${y}
+      C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+      Z`;
+    };
+    
+    const TriangleBar = (props) => {
+      const { fill, x, y, width, height } = props;    
+      return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
   return (
-    <div>
-      <h2>Pages To Read route</h2>
+    <div className=" bg-rose-50 rounded-2xl mt-10 flex justify-center py-12">
+      
        <BarChart
-        width={500}
-        height={300}
+        width={1200}
+        height={600}
           data={readBooks} 
         margin={{
           top: 20,
@@ -40,9 +60,9 @@ const PagesToRead = () => {
         <Tooltip></Tooltip>
         
         <Bar
-        //   dataKey="uv"  here the value which I want to show
+           dataKey="totalPages" 
           fill="#8884d8"
-          //  shape={<TriangleBar />}
+          shape={<TriangleBar />}
           
           label={{ position: "top" }}
         >
@@ -51,8 +71,19 @@ const PagesToRead = () => {
           ))}
         </Bar>
       </BarChart> 
+
+
+      
     </div>
   );
 };
 
+PagesToRead.propTypes = {
+  props: PropTypes.object,
+  fill: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
 export default PagesToRead;
